91fcc936dca357076943d1a39776957dec4da7b4
Alex Wang <jianuo.wang@duke.edu>
2020-06-03 00:51:16 -0500

    selector labels

--- src/ControlBar/ControlBar.js
+++ src/ControlBar/ControlBar.js
@@ -9,24 +9,32 @@ function ControlBar(props) {
                 <h1>Visualizer</h1>
             </div>
 
-            <div className="select control-bar-selectors" id="algo-selector">
-                <select name="algorithm" value={props.algorithm} onChange={props.handleFormChange}> 
-                    <option value="dfs">Depth-First Search</option>
-                    <option value="bfs">Breadth-First Search</option>
-                    <option value="dijkstra">Dijkstra</option>
-                    <option value="bds">Bi-Directional Search</option>
-                </select>
+            <div className="select-container">
+                <label className="select-label">Maze Type</label>
+                <div className="select control-bar-selectors" id="maze-selector">
+                    <select name="maze" value={props.maze} onChange={props.handleFormChange}> 
+                        <option value="rmd">Recursive Maze Division</option>
+                        <option value="rmd-vs">Vertical Skew</option>
+                        <option value="rmd-hs">Horizontal Skew</option>
+                    </select>
+                </div>
             </div>
-
-            <div className="select control-bar-selectors" id="maze-selector">
-                <select name="maze" value={props.maze} onChange={props.handleFormChange}> 
-                    <option value="rmd">Recursive Maze Division</option>
-                    <option value="rmd-vs">Vertical Skew</option>
-                    <option value="rmd-hs">Horizontal Skew</option>
-                </select>
-            </div>
-
+    
             <button className="button" id="maze-button" onClick={props.generateMaze}>Generate Maze</button>
+
+            <div className="select-container">
+            <label className="select-label">Algorithm</label>
+                <div className="select control-bar-selectors" id="algo-selector">
+                    <select name="algorithm" value={props.algorithm} onChange={props.handleFormChange}> 
+                        <option value="dfs">Depth-First Search</option>
+                        <option value="bfs">Breadth-First Search</option>
+                        <option value="dijkstra">Dijkstra</option>
+                        <option value="bds">Bi-Directional Search</option>
+                    </select>
+                </div> 
+            </div>
+
+            <button className="button" id="visualize-button" onClick={props.visualize}>Visualize {props.algorithm.toUpperCase()}</button>
 
             <div className="control-bar-selectors" id="mark-selector">
                 <select name="marker" value={props.marker} onChange={props.handleFormChange}> 
@@ -36,24 +44,32 @@ function ControlBar(props) {
                 </select>
             </div>
 
-            <div className="select control-bar-selectors" id="speed-selector">
-                <select name="speed" value={props.speed} onChange={props.handleFormChange}> 
-                    <option value="slow">Slow</option>
-                    <option selected="selected" value="moderate">Moderate</option>
-                    <option value="fast">Fast</option>
-                </select>
+            <div className="select-container">
+                <label className="select-label">Speed</label>
+                <div className="select control-bar-selectors" id="speed-selector">
+                    <select name="speed" value={props.speed} onChange={props.handleFormChange}> 
+                        <option value="slow">Slow</option>
+                        <option selected="selected" value="moderate">Moderate</option>
+                        <option value="fast">Fast</option>
+                    </select>
+                </div> 
             </div>
+    
 
-            <div className="control-bar-selectors" id="grid-selector">
-                <label>
-                    # Rows <input type="text" name="rowInput" value={props.rowInput} onChange={props.handleFormChange}/>
-                </label>
-            </div>
+            <div>
+                <div className="control-bar-selectors" id="grid-selector">
+                    <label>
+                        # Rows 
+                        <input type="text" name="rowInput" value={props.rowInput} onChange={props.handleFormChange}/>
+                    </label>
+                </div>
 
-            <div className="control-bar-selectors" id="grid-selector">
-                <label>
-                    # Columns <input type="text" name="colInput" value={props.colInput} onChange={props.handleFormChange}/>
-                </label>
+                <div className="control-bar-selectors" id="grid-selector">
+                    <label>
+                        # Columns 
+                        <input type="text" name="colInput" value={props.colInput} onChange={props.handleFormChange}/>
+                    </label>
+                </div>
             </div>
 
             <div className="control-bar-selectors" id="grid-selector">
@@ -64,7 +80,6 @@ function ControlBar(props) {
                 <button className="button" name="clearButton" onClick={props.handleFormButton}>Clear Grid</button>
             </div>
 
-            <button className="button" id="visualize-button" onClick={props.visualize}>Visualize!</button>
         </header>
     )
 }
